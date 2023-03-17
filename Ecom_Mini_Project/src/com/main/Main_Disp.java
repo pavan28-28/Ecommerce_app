f3c3b562caaadadcd9f8df1d679237d3273bbdb0
pavan28-28 <pavan2841999@gmail.com>
2023-03-17 16:55:14 +0530

    fourth commit

--- Ecom_Mini_Project/src/com/main/Main_Disp.java
+++ Ecom_Mini_Project/src/com/main/Main_Disp.java
@@ -6,14 +6,15 @@ public class Main_Disp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Scanner x=new Scanner(System.in);
+		Scanner x=new Scanner(System.in);		
+		int ch,ch1;
 		
-		
+		do {
 		System.out.println("******** >> Welcome To Ecommerse Application << ********");
 		
-		int ch=0,ch1=0;
 		
-		System.out.println("\n 1-Registered User  \n 2-Admin \n 3-Guest User");
+		
+		System.out.println("\n 1-Registered User  \n 2-Admin \n 3-Guest User \n 4-Exit");
 		
 		System.out.println("Please Select your Status");
 		ch=x.nextInt();
@@ -26,9 +27,9 @@ public class Main_Disp {
 			break;
 			
 		case 2:
-			
+			do {
 			System.out.println("\n 1-Add Product item \n 2-Calculate Bill \n 3-Display Amount To End User "
-					+ "\n 4-Check Quantity  \n 5-check Registered Users \n 6-Check The Particular History");
+					+ "\n 4-Check Quantity  \n 5-check Registered Users \n 6-Check The Particular History \n 7-Exit");
 			
 			System.out.println("Select your choice");
 			ch1=x.nextInt();
@@ -78,21 +79,29 @@ public class Main_Disp {
 			 case 6:
 				 
 				 break;
+				 
+			 case 7:
+				 System.out.println();
+				 break;
 			default:
-				
+				System.out.println("Enter the Wrong Choice Enter again");
 				 
 			 }
+			}while(ch1!=7);
 			break;
 			
 		case 3:
 			
 			break;
-		default:	
+		
+		case 4:
+			System.out.println("\n\n *****THank You*****");
+			break;
+		default:
+			System.out.println("Wrong choice enter again");
+		
 		}
-		
-		
-
-	
+		}while(ch!=4);
 		
 	}
 
